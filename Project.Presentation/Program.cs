47bb4f14fb226f7c9d4b03068ea9518dad3faf4c
angelstchavez <angelstchavez@gmail.com>
2024-01-18 23:28:26 -0500

    feat: MV

--- Project.Presentation/Program.cs
+++ Project.Presentation/Program.cs
@@ -13,6 +13,7 @@ namespace Project.Presentation
         [STAThread]
         static void Main()
         {
+            //MV
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new LoginForm());
